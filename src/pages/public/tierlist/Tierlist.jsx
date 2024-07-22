a2cf262a3e6bcb2ee669169278d5646e13f1928e
JunhyV <jonhy.kisi@gmail.com>
2024-07-22 00:35:56 -0600

    tierlist format

--- src/pages/public/tierlist/Tierlist.jsx
+++ src/pages/public/tierlist/Tierlist.jsx
@@ -1,11 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Tier from "../../../components/public/Tier";
 
 const Tierlist = () => {
+  const tiers = ["SS", "S", "A", "B", "C"];
+
   return (
-    <div className="bg-neutral-800 bg-opacity-80 min-h-full">
-      <h1 className="font-black text-3xl text-white text-center p-2">
-        Tier List
-      </h1>
+    <div className="bg-neutral-800 bg-opacity-80 min-h-full text-white">
+      <div className="text-white p-2">
+        <h1 className="font-black text-3xl text-center mb-2 ">
+          Zenless Zone Zero Tier List
+        </h1>
+        <p className="mb-2">
+          <Link to="/" className="hover:text-yellow-500">
+            Home
+          </Link>
+          /
+          <Link to="/tierlist" className="hover:text-yellow-500">
+            Tier List
+          </Link>
+        </p>
+        <hr className="border-yellow-500" />
+        <p className="mt-2">
+          The continuous tier list was created with
+          <span className="font-black">Zenless Zone Zero</span> (ZZZ) version
+          <span className="font-black"> 1.0</span> in mind. - There could be
+          changes, though it is too early to establish criteria for all
+          characters at the moment. We hope you enjoy the content. We're working
+          to give you the best experience in the game.
+        </p>
+        <p>
+          <span className="font-thin">Last updated: </span>
+          {new Date().getFullYear() +
+            "." +
+            (new Date().getMonth() + 1).toString().padStart(2, "0") +
+            "." +
+            new Date().getDate()}
+        </p>
+      </div>
+
+      <div className="p-2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus
+        animi, ex sequi magnam, dolor, iusto nisi alias ullam natus voluptate
+        explicabo eligendi ipsam dolore? Saepe beatae odit eaque minima quia!
+        Velit voluptatum nulla recusandae eligendi eos tenetur quo, iure
+        quisquam earum aspernatur pariatur a dolores soluta unde, vel corrupti
+        sequi.
+      </div>
+
+      <div className="p-2">
+        <div className="flex bg-neutral-800">
+          <div className="w-10 text-center">Tier</div>
+          <div className="grid grid-cols-4 w-full text-center font-medium">
+            <p className="text-purple-400">DMG DPS</p>
+            <p className="text-sky-400">ANOMALY DPS</p>
+            <p className="text-red-400">STUN & DEFENSE</p>
+            <p className="text-green-400">SUPPORT</p>
+          </div>
+        </div>
+        {tiers.map((tier) => (
+          <Tier rank={tier} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/utils/tierlist.js
@@ -0,0 +1,37 @@
+const tierList = [
+    {
+        rank: 'SS',
+        dmg_carry: ['ellen'],
+        anomaly_carry: ['grace'],
+        stun: ['lycaon'],
+        support: ['soukaku', 'nicole']
+    },
+    {
+        rank: 'S',
+        dmg_carry: ['nekomata', 'soldier 11'],
+        anomaly_carry: ['piper'],
+        stun: ['koleda'],
+        support: ['lucy']
+    },
+    {
+        rank: 'A',
+        dmg_carry: [ 'corin'],
+        anomaly_carry: [],
+        stun: ['anby'],
+        support: ['rina']
+    },
+    {
+        rank: 'B',
+        dmg_carry: ['anton'],
+        anomaly_carry: [],
+        stun: [],
+        support: []
+    },
+    {
+        rank: 'C',
+        dmg_carry: ['billy'],
+        anomaly_carry: [],
+        stun: ['ben'],
+        support: []
+    }
+]
