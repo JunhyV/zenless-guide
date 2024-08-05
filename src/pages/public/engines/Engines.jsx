cae29cba470f1ced3b4d2b2254422ae4b2e962cb
chito <chito.pepsi@gmail.com>
2024-08-05 11:29:37 -0600

    w-engines page

--- src/pages/public/engines/Engines.jsx
+++ src/pages/public/engines/Engines.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { EnginesCard } from "../../../components/engines/EnginesCard";
+import { apiCall } from "../../../utils/apiCall.js";
+import { extractStats } from "../../../utils/extras/w-engines-Funcion";
 
 const Engines = () => {
-  return (
-    <div className="bg-neutral-800 bg-opacity-80 min-h-full">
-      <h1 className="font-black text-3xl text-white text-center p-2">
-        W-ENGINES
-      </h1>
-    </div>
-  )
-}
+  const [engines, setEngines] = useState([]);
 
-export default Engines
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await apiCall("https://zenless-api.vercel.app/engines");
+        const processedData = data.map((engine) => {
+          const { baseAttack, secondaryStat } = extractStats(engine.stats);
+          return {
+            ...engine,
+            baseAttack,
+            secondaryStat,
+          };
+        });
+        setEngines(processedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div className=" min-h-screen p-8">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold text-center">Zenless Zone Zero W-Engines</h1>
+        <p className="text-center text-gray-700">Last updated: 2024.07.15</p>
+      </header>
+      <div className="flex flex-wrap justify-center">
+        {engines.map((engine) => (
+          <EnginesCard
+            key={engine._id}
+            name={engine.name}
+            type={`${engine.rol} - ${engine.rank}`}
+            baseAttack={engine.baseAttack}
+            secondaryStat={engine.secondaryStat}
+            description={engine.effect}
+            image={engine.img}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Engines;
