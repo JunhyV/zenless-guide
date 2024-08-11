946c96536bfb78ced6ba297afcf680c9503d2186
JunhyV <jonhy.kisi@gmail.com>
2024-08-10 20:55:38 -0600

    fix

--- src/pages/public/engines/Engines.jsx
+++ src/pages/public/engines/Engines.jsx
@@ -3,39 +3,17 @@ import { EnginesCard } from "../../../components/engines/EnginesCard";
 import { apiCall } from "../../../utils/apiCall.js";
 import { extractStats } from "../../../utils/extras/w-engines-Funcion";
 import { Header } from "../../../components/header/Header.jsx";
-import { notReleasedEngine } from "../../../utils/gameVersion.js";
 
 const Engines = () => {
   const [engines, setEngines] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       let data;
 
       try {
-        const res = await apiCall("https://zenless-api.vercel.app/engines");
-        console.log(res);
-        
-
-        // Filter bangboos
-        const noYet = res.filter((engine) =>
-          notReleasedEngine.some((name) => engine.name.includes(name))
-        );
-        
-
-        if (noYet.length !== 0) {
-          data = res.filter(
-            (engine) =>
-              !notReleasedEngine.some((name) => engine.name.includes(name))
-          );
-          
-        } else {
-          data = res;
-        }
-
-        // Order Array by name
-        data.sort((a, b) => a.name.localeCompare(b.name));
-
+        const data = await apiCall("https://zenless-api.vercel.app/engines");
         const processedData = data.map((engine) => {
           const { baseAttack, secondaryStat } = extractStats(engine.stats);
           return {
@@ -47,6 +25,8 @@ const Engines = () => {
         setEngines(processedData);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -55,19 +35,21 @@ const Engines = () => {
 
   return (
     <div className="min-h-screen  bg-neutral-800 bg-opacity-80">
-      <Header pages="w-engines" link={"engines"} />
+      <Header pages="w-engines" link={'engines'}/>
       <div className="flex flex-wrap justify-center items-center">
-        {engines.map((engine) => (
-          <EnginesCard
-            key={engine._id}
-            name={engine.name}
-            type={`${engine.rol} - ${engine.rank}`}
-            baseAttack={engine.baseAttack}
-            secondaryStat={engine.secondaryStat}
-            description={engine.effect}
-            image={engine.img}
-          />
-        ))}
+        {loading
+          ? Array.from({ length: 6 }).map((_, index) => <SkeletonEngines key={index} />)
+          : engines.map((engine) => (
+              <EnginesCard
+                key={engine._id}
+                name={engine.name}
+                type={`${engine.rol} - ${engine.rank}`}
+                baseAttack={engine.baseAttack}
+                secondaryStat={engine.secondaryStat}
+                description={engine.effect}
+                image={engine.img}
+              />
+            ))}
       </div>
     </div>
   );
